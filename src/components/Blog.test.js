272d9ee2726e09cf7d417d7c1b5f8b4a56d26008
HeXudong <1514133606@qq.com>
2023-01-18 20:51:01 +0800

    test: add blog test

---
+++ src/components/Blog.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+const user = {
+  name: 'Superuser',
+  token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpZCI6IjYzYzdiMTA2M2FlODk3OGZmYjM1MDZlZSIsImlhdCI6MTY3NDA0NDE4NCwiZXhwIjoxNjc0MjE2OTg0fQ.bqEcB1Ntt2jwHk86jMlVo-UsLdcFkhpE1tch3VzJoy0',
+  username: 'test'
+}
+
+const canRemove = (blog) => blog.user && blog.user.username === user.username
+
+
+test('renders content', () => {
+  const blog = {
+    title: 'My Book',
+    author: 'AAA',
+    url: '111',
+    user: {
+      username: 'test',
+      name: 'Superuser',
+      blogs: [
+        '63c7c2523ae8978ffb350764'
+      ],
+      id: '63c7b1063ae8978ffb3506ee'
+    },
+    likes: 9,
+    id: '63c7c2523ae8978ffb350764'
+  }
+
+  render(<Blog blog={blog} canRemove={canRemove} />)
+
+  const element = screen.getByText('My Book')
+  expect(element).toBeDefined()
+})
